3767904a188b103931c88553c2f120d39a7274c1
Cherry <cherry@Chariths-MacBook-Air.local>
2026-05-16 11:20:15 +0530

    Ready for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/Chitra_Kala/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
